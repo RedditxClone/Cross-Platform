c8f0947c34a461561f0cc1ded3654fbb57cdbf69
Bemoi Erian <bemoi.erian@gmail.com>
2022-10-22 16:06:58 +0200

    Initial project files

---
+++ reddit/lib/app_router.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  // declare repository and cubit objects
+  AppRouter() {
+    // initialise repository and cubit objects
+  }
+  Route? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(
+            builder: (_) => Scaffold(
+                  appBar: AppBar(),
+                  body: Container(),
+                ));
+      /*
+      case example:
+      case '/':
+        return MaterialPageRoute(
+          // create bloc at the the root of widget tree
+          builder: (_) => BlocProvider(
+            // we pass the cubit class to create
+            create: (context) => CubitObject,
+            // our child is the screen itself
+            child: const Screen(),
+          ),
+        );
+      */
+      default:
+        return null;
+    }
+  }
+}
---
+++ reddit/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'app_router.dart';
+
+void main() {
+  runApp(MyApp(appRouter: AppRouter()));
+}
+
+class MyApp extends StatelessWidget {
+  final AppRouter appRouter;
+
+  const MyApp({Key? key, required this.appRouter}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Reddit',
+      theme: ThemeData(
+        // This is the theme of your application.
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: appRouter.generateRoute,
+    );
+  }
+}
