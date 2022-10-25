f00a63d7e5d749965bd0a68de662c13d3dfa1bc7
mark yasser <markyasser2011@gmail.com>
2022-10-25 17:09:51 +0200

    restored app router

--- reddit/lib/app_router.dart
+++ reddit/lib/app_router.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:reddit/presentation/screens/home.dart';
 
 class AppRouter {
   // declare repository and cubit objects
@@ -9,7 +8,10 @@ class AppRouter {
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
-        return MaterialPageRoute(builder: (_) => const Home());
+        return MaterialPageRoute(builder: (_) => Scaffold(
+                  appBar: AppBar(),
+                  body: Container(),
+                ));
       /*
       case example:
       case '/':
--- reddit/lib/presentation/screens/home.dart
+++ reddit/lib/presentation/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:reddit/presentation/screens/signup_screen.dart';
 
+//for testing and will be deleted
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
