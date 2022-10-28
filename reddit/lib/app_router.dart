8e735cce35391dc8d993be88b60562206c18a22b
rufaida-kassem <rufaidahkassem@gmail.com>
2022-10-28 16:56:14 +0200

    routing

--- reddit/lib/app_router.dart
+++ reddit/lib/app_router.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reddit/business_logic/bloc/email_settings_bloc.dart';
+import 'package:reddit/data/repository/email_settings_repo.dart';
+import 'presentation/screens/email_settings_web.dart';
+import 'presentation/screens/home_page.dart';
+import 'presentation/screens/safety_settings_web.dart';
 
 class AppRouter {
   // declare repository and cubit objects
+  late EmailSettingsReposity emailSettingsReposity;
+  late EmailSettingsBloc emailSettingsBloc;
+
   AppRouter() {
     // initialise repository and cubit objects
+    emailSettingsReposity = EmailSettingsReposity();
+    emailSettingsBloc = EmailSettingsBloc(emailSettingsReposity);
   }
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
+        return MaterialPageRoute(builder: (_) => const HomePage());
+
+      case '/emailSettings':
         return MaterialPageRoute(
-            builder: (_) => Scaffold(
-                  appBar: AppBar(),
-                  body: Container(),
+            builder: (_) => BlocProvider.value(
+                  value: emailSettingsBloc,
+                  child: const EmailSettingsWeb(userId: 1),
                 ));
       /*
       case example:
