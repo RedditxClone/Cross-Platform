df98a120bfc3be015e42a1269d94648b341f7394
mark yasser <markyasser2011@gmail.com>
2022-10-31 00:41:23 +0200

    profile settings empty test file

---
+++ reddit/test/profile_settings_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('testing get request', () {});
+  group('testing patch requests', () {
+    test('change cover photo', () {});
+    test('change profile photo', () {});
+    test('change display name', () {});
+    test('change about', () {});
+    test('change allowPeopleToFollowYou', () {});
+    test('change activeInCommunitiesVisibility', () {});
+    test('change contentVisibility', () {});
+    test('change nsfw', () {});
+  });
+}
