ce08f2d8872438f4809c1f73ba338c1db389ae8b
mark yasser <markyasser2011@gmail.com>
2022-10-31 01:11:20 +0200

    empty test files

---
+++ reddit/test/safety_settings_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('testing get request', () {});
+  group('testing patch requests', () {
+    test('change disroptiveSettings', () {});
+    test('change showUnInSearch', () {});
+    test('change personalizeAllOfReddit', () {});
+    test('change personalizeAds_information', () {});
+    test('change personalizeAds_yourActivity', () {});
+    test('change personalizeRec_generalLocation', () {});
+    test('change personalizeRec_ourPartners', () {});
+    test('change useTwoFactorAuthentication', () {});
+  });
+}
