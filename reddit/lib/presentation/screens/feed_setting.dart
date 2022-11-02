36826c267f9eb089437d7bdfba6208a7bda2d503
omarkhaled2001 <omarkh200165@gmail.com>
2022-11-02 23:12:38 +0200

    ONLY UI coding & Remains:{Comments,Unit Test,Documentation}

---
+++ reddit/lib/presentation/screens/feed_setting.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class FeedSetting extends StatelessWidget {
+  const FeedSetting({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
