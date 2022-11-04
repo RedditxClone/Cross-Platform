312c7606a1f5d80c6e6c4e77be5bee63c7a66eb5
rufaida-kassem <rufaidahkassem@gmail.com>
2022-11-04 03:55:53 +0200

    route constant

--- reddit/lib/app_router.dart
+++ reddit/lib/app_router.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:reddit/business_logic/cubit/email_settings_cubit.dart';
 import 'package:reddit/data/repository/email_settings_repo.dart';
 import 'package:reddit/data/web_services/email_settings_web_services.dart';
+import 'constants/strings.dart';
 import 'presentation/screens/email_settings_web.dart';
 import 'presentation/screens/home_page.dart';
 
@@ -23,7 +24,7 @@ class AppRouter {
       case '/':
         return MaterialPageRoute(builder: (_) => const HomePage());
 
-      case '/emailSettings':
+      case emailSettingsWebScreenRoute:
         return MaterialPageRoute(
             builder: (_) => BlocProvider.value(
                   value: emailSettingsCubit,
--- reddit/lib/constants/strings.dart
+++ reddit/lib/constants/strings.dart
@@ -1,1 +1,2 @@
 const baseUrl = 'https://f1c179b0-0158-4a47-ba39-7b803b8ae58a.mock.pstmn.io';
+const emailSettingsWebScreenRoute = "/emailSettings";
