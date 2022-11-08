300b0890426ee723fd039875a711b2fe4a115d7a
omarkhaled2001 <omarkh200165@gmail.com>
2022-11-08 10:54:10 +0200

    Make it support both Dark Mode & Light Mode

--- reddit/lib/presentation/screens/setting_tab_ui.dart
+++ reddit/lib/presentation/screens/setting_tab_ui.dart
@@ -1,4 +1,3 @@
-import 'package:dartdoc/dartdoc.dart';
 import 'package:flutter/material.dart';
 
 class SettingTabUi extends StatefulWidget {
@@ -9,44 +8,65 @@ class SettingTabUi extends StatefulWidget {
 }
 
 class _SettingTabUiState extends State<SettingTabUi> {
+  var whiteColor = Colors.white;
+  var blackColor = Colors.black;
+
+  late var labelColor = blackColor;
+
+  late var backgroundColor = whiteColor;
+
+  void switchMode() {
+    if (backgroundColor == blackColor) {
+      labelColor = blackColor;
+      backgroundColor = whiteColor;
+    } else {
+      labelColor = whiteColor;
+      backgroundColor = blackColor;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 5,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(150, 10, 150, 0),
-        child: Scaffold(
-          appBar: AppBar(
-            automaticallyImplyLeading: false,
-            backgroundColor: Colors.white,
-            title: const Text(
-              "User settings",
-              style: TextStyle(color: Colors.black),
+      child: Container(
+        color: backgroundColor,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(150, 10, 150, 0),
+          child: Scaffold(
+            backgroundColor: backgroundColor,
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: backgroundColor,
+              title: Text(
+                "User settings",
+                style: TextStyle(color: labelColor),
+              ),
+              bottom: TabBar(
+                labelColor: labelColor,
+                unselectedLabelColor: const Color.fromARGB(255, 131, 122, 122),
+                tabs: const <Widget>[
+                  Tab(
+                    child: Text("Account"),
+                  ),
+                  Tab(
+                    child: Text("Profile"),
+                  ),
+                  Tab(
+                    child: Text("Safety and privacy"),
+                  ),
+                  Tab(
+                    child: Text("Feed Settings"),
+                  ),
+                  Tab(
+                    child: Text("Email"),
+                  ),
+                ],
+              ),
             ),
-            bottom: const TabBar(
-              labelColor: Colors.black,
-              unselectedLabelColor: Color.fromARGB(255, 28, 28, 28),
-              tabs: <Widget>[
-                Tab(
-                  child: Text("Account"),
-                ),
-                Tab(
-                  child: Text("Profile"),
-                ),
-                Tab(
-                  child: Text("Safety and privacy"),
-                ),
-                Tab(
-                  child: Text("Feed Settings"),
-                ),
-                Tab(
-                  child: Text("Email"),
-                ),
-              ],
+            body: const TabBarView(
+              children: <Widget>[],
             ),
-          ),
-          body: const TabBarView(
-            children: <Widget>[],
           ),
         ),
       ),
