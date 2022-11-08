13d4817cd3784dfa54d5c5923d8f72c1d08dcb6a
omarkhaled2001 <omarkh200165@gmail.com>
2022-11-08 11:55:33 +0200

    Make it support both Dark Mode & Light Mode

--- reddit/lib/presentation/screens/feed_setting.dart
+++ reddit/lib/presentation/screens/feed_setting.dart
@@ -9,10 +9,47 @@ class FeedSetting extends StatefulWidget {
 class FeedSettingState extends State<FeedSetting> {
   bool _isActiveAdultContent = false;
   bool _isActiveAutoplayMedia = false;
+
+  var whiteColor = Colors.white;
+  var blackColor = Colors.black;
+  var lightGrayColor0 = const Color.fromARGB(255, 207, 201, 201);
+  var lightGrayColor1 = const Color.fromARGB(255, 125, 124, 124);
+  var darkGrayColor = const Color.fromARGB(255, 34, 33, 33);
+
+  late var trackColor = lightGrayColor0;
+
+  late var titleColor = blackColor;
+
+  late var subTitleColor = lightGrayColor1;
+
+  late var backgroundColor = whiteColor;
+
+  void switchMode() {
+    if (backgroundColor == blackColor) {
+      ///Light Mode
+      titleColor = blackColor;
+      subTitleColor = lightGrayColor1;
+      backgroundColor = whiteColor;
+      trackColor = lightGrayColor0;
+    } else {
+      ///Dark Mode
+      titleColor = whiteColor;
+      subTitleColor = lightGrayColor1;
+      backgroundColor = blackColor;
+      trackColor = darkGrayColor;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      // ///For testing switching between modes only.
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () => setState(() {
+      //     switchMode();
+      //   }),
+      // ),
+      backgroundColor: backgroundColor,
       body: Padding(
         padding: const EdgeInsets.fromLTRB(150, 40, 600, 200),
         child: ListView(
@@ -21,19 +58,30 @@ class FeedSettingState extends State<FeedSetting> {
               margin: const EdgeInsets.only(bottom: 40),
               child: Text(
                 "Feed settings",
-                style: Theme.of(context).textTheme.headline5!,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline5!
+                    .merge(TextStyle(color: titleColor)),
               ),
             ),
             Text(
               "CONTENT PREFERENCES",
-              style: Theme.of(context).textTheme.caption!,
+              style: Theme.of(context)
+                  .textTheme
+                  .caption!
+                  .merge(TextStyle(color: subTitleColor)),
             ),
             const Divider(),
             SwitchListTile(
-              inactiveTrackColor: Colors.grey.shade400,
-              title: const Text("Adult content"),
-              subtitle: const Text(
-                  "Enable to view adult and NSFW (not safe for work) content in your feed and search results."),
+              inactiveTrackColor: trackColor,
+              title: Text(
+                "Adult content",
+                style: TextStyle(color: titleColor),
+              ),
+              subtitle: Text(
+                "Enable to view adult and NSFW (not safe for work) content in your feed and search results.",
+                style: TextStyle(color: subTitleColor),
+              ),
               isThreeLine: true,
               value: _isActiveAdultContent,
               onChanged: (value) {
@@ -43,10 +91,15 @@ class FeedSettingState extends State<FeedSetting> {
               },
             ),
             SwitchListTile(
-              inactiveTrackColor: Colors.grey.shade400,
-              title: const Text("Autoplay media"),
-              subtitle: const Text(
-                  "Play videos and gifs automatically when in the viewport."),
+              inactiveTrackColor: trackColor,
+              title: Text(
+                "Autoplay media",
+                style: TextStyle(color: titleColor),
+              ),
+              subtitle: Text(
+                "Play videos and gifs automatically when in the viewport.",
+                style: TextStyle(color: subTitleColor),
+              ),
               isThreeLine: true,
               value: _isActiveAutoplayMedia,
               onChanged: (value) {
