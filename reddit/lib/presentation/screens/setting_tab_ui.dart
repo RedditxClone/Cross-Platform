27c38b10b0d55acf2153cceb5fcb60173f15c5d7
omarkhaled2001 <omarkh200165@gmail.com>
2022-11-08 16:25:01 +0200

    Remove support both Light Mode

--- reddit/lib/presentation/screens/setting_tab_ui.dart
+++ reddit/lib/presentation/screens/setting_tab_ui.dart
@@ -8,57 +8,27 @@ class SettingTabUi extends StatefulWidget {
 }
 
 class _SettingTabUiState extends State<SettingTabUi> {
-  var whiteColor = Colors.white;
-  var blackColor = Colors.black;
-
-  ///Its value changes according to Application Mode.
-  ///
-  ///Default is Light Mode.
-  late var labelColor = blackColor;
-
-  ///Its value changes according to Application Mode.
-  ///
-  ///Default is Light Mode.
-  late var backgroundColor = whiteColor;
-
-  ///Changing the dark mode to light mode versa.
-  void switchMode() {
-    if (backgroundColor == blackColor) {
-      labelColor = blackColor;
-      backgroundColor = whiteColor;
-    } else {
-      labelColor = whiteColor;
-      backgroundColor = blackColor;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 5,
       child: Container(
-        color: backgroundColor,
+        color: Colors.black,
         child: Padding(
           padding: const EdgeInsets.fromLTRB(150, 10, 150, 0),
           child: Scaffold(
-            // ///For testing switching between modes only.
-            // floatingActionButton: FloatingActionButton(
-            //   onPressed: () => setState(() {
-            //     switchMode();
-            //   }),
-            // ),
-            backgroundColor: backgroundColor,
+            backgroundColor: Colors.black,
             appBar: AppBar(
               automaticallyImplyLeading: false,
-              backgroundColor: backgroundColor,
-              title: Text(
+              backgroundColor: Colors.black,
+              title: const Text(
                 "User settings",
-                style: TextStyle(color: labelColor),
+                style: TextStyle(color: Colors.white),
               ),
-              bottom: TabBar(
-                labelColor: labelColor,
+              bottom: const TabBar(
+                labelColor: Colors.white,
                 unselectedLabelColor: const Color.fromARGB(255, 131, 122, 122),
-                tabs: const <Widget>[
+                tabs: <Widget>[
                   Tab(
                     child: Text("Account"),
                   ),
