750473d62ab23a017235db33416b78f123ff9543
Bemoi Erian <bemoi.erian@gmail.com>
2022-11-12 20:17:19 +0200

    base url can now be taken by command line argument

--- reddit/lib/constants/strings.dart
+++ reddit/lib/constants/strings.dart
@@ -2,7 +2,7 @@
 // --------------Backend-----------------
 //---------------------------------------
 // Backend URL
-const baseUrl = '';
+const baseUrl = String.fromEnvironment('BASE_URL', defaultValue: '');
 const mockUrl = 'https://a3a539c4-1746-44d4-8e06-d579a1d30d53.mock.pstmn.io/';
 // Use mock server instead of our backend server
 const useMockServerForAllWebServices = true;
