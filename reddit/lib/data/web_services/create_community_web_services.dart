9d48ce9af717b6fe0df4e067236fd512da660f6f
rufaida-kassem <rufaidahkassem@gmail.com>
2022-11-16 18:23:10 +0200

    web services

---
+++ reddit/lib/data/web_services/create_community_web_services.dart
@@ -0,0 +1,49 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:reddit/constants/strings.dart';
+
+class CreateCommunityWebServices {
+  bool useMockServer = true;
+  String mockUrl = "https://f1c179b0-0158-4a47-ba39-7b803b8ae58a.mock.pstmn.io";
+  late Dio dio;
+  CreateCommunityWebServices() {
+    BaseOptions options = BaseOptions(
+      baseUrl: useMockServer ? mockUrl : baseUrl,
+      receiveDataWhenStatusError: true,
+      connectTimeout: 20 * 1000, //20 secs
+      receiveTimeout: 20 * 1000,
+    );
+    dio = Dio(options);
+  }
+
+  /// Sends create community request to the server, the repository calls this function
+  Future<void> createCommunity(Map<String, dynamic> communityData) async {
+    try {
+      Response response = await dio.post(
+        'subreddit',
+        data: communityData,
+      );
+      if (response.statusCode == 201) {
+        debugPrint("subreddit successfully");
+      } else {
+        debugPrint("Failed to create subreddit");
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  Future<bool> getIfNameAvailable(String subredditName) async {
+    try {
+      Response response = await dio.get(
+        'subreddit/r/$subredditName/available',
+      );
+      if (response.statusCode == 200) {
+        return true;
+      } else {}
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+    return false;
+  }
+}
