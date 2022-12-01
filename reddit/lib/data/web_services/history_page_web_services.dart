d42d3d00ee432d6ecf27bdcb0230f4d0985b7d56
rufaida-kassem <rufaidahkassem@gmail.com>
2022-12-01 23:25:59 +0200

    base url

--- reddit/lib/data/web_services/history_page_web_services.dart
+++ reddit/lib/data/web_services/history_page_web_services.dart
@@ -9,7 +9,7 @@ class HistoryPageWebServices {
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODhhNjFiNWUwYjU4M2Y0YTc5ZTQxYSIsImlhdCI6MTY2OTg5OTgwMywiZXhwIjoxNjcwNzYzODAzfQ.19uD_QlcThGaS_lZ0iE92q0771WwJSB2jgWfJPTWkn8";
   HistoryPageWebServices() {
     BaseOptions options = BaseOptions(
-      baseUrl: "https://swproject.demosfortest.com/api/",
+      baseUrl: baseUrl,
       receiveDataWhenStatusError: true,
       connectTimeout: 20 * 1000, //20 secs
       receiveTimeout: 20 * 1000,
