26d750c1a89f9597ea7b29f1abacda4345ad96f3
omarkhaled2001 <omarkh200165@gmail.com>
2022-12-20 21:30:29 +0200

    Ready to Real API BackEnd Calls

--- reddit/lib/data/web_services/message_screen_web_services.dart
+++ reddit/lib/data/web_services/message_screen_web_services.dart
@@ -1,24 +1,34 @@
 // ignore_for_file: avoid_print, duplicate_ignore
 
 import 'package:dio/dio.dart';
+import 'package:reddit/constants/strings.dart';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:flutter/cupertino.dart';
+import 'package:http_parser/http_parser.dart';
 
 class MessageScreenWebServices {
   late Dio dio;
-
+  String token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODhhNjFiNWUwYjU4M2Y0YTc5ZTQxYSIsImlhdCI6MTY2OTg5OTgwMywiZXhwIjoxNjcwNzYzODAzfQ.19uD_QlcThGaS_lZ0iE92q0771WwJSB2jgWfJPTWkn8";
+  //String mockUrl = "https://43a82e45-5f65-4651-90fe-a2190fb44ded.mock.pstmn.io/";
   ///Setting Dio Options.
   MessageScreenWebServices() {
     BaseOptions options = BaseOptions(
-      baseUrl: "https://43a82e45-5f65-4651-90fe-a2190fb44ded.mock.pstmn.io/",
+      baseUrl: baseUrl,
       receiveDataWhenStatusError: true,
       connectTimeout: 20 * 1000, //20 secs
       receiveTimeout: 20 * 1000,
     );
     dio = Dio(options);
   }
-
   dynamic getAllSentMessages() async {
     try {
-      Response response = await dio.get('message/me/sent');
+      Response response = await dio.get('message/me/sent',
+          options: Options(
+            headers: {"Authorization": "Bearer $token"},
+          ));
+      debugPrint('status code : ${response.statusCode}');
       print(response.data);
       return response.data;
     } catch (e) {
@@ -29,7 +39,11 @@ class MessageScreenWebServices {
 
   dynamic getAllMessagesInbox() async {
     try {
-      Response response = await dio.get('message/me/message');
+      Response response = await dio.get('message/me/message',
+          options: Options(
+            headers: {"Authorization": "Bearer $token"},
+          ));
+      debugPrint('status code : ${response.statusCode}');
       print(response.data);
       return response.data;
     } catch (e) {
