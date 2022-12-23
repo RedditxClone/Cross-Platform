741599d95de17099988544879a0937101e3edeb6
Bemoi Erian <bemoi.erian@gmail.com>
2022-12-23 03:09:56 +0200

    Added functional docs

--- reddit/lib/data/repository/comments/comments_repository.dart
+++ reddit/lib/data/repository/comments/comments_repository.dart
@@ -11,6 +11,7 @@ class CommentsRepository {
 
   CommentsRepository(this.commentsWebServices);
 
+  /// [id] of post/comment that we want to get its comments/replies
   /// Returns [List] of [Comments] object that contains the comments of post or comment [id]
   /// after getting it from [CommentsWebServices] and mapping it to the model list.
   Future<List<Comments>> getThingComments(String id) async {
@@ -20,7 +21,8 @@ class CommentsRepository {
     return List<Comments>.from(comments.map((i) => Comments.fromJson(i)));
   }
 
-  /// Map new comment model to Json and send it to web services [CommentsWebServices.addComment] to make the POST request.
+  /// [commentSubmit] Model containin the info of the comment that will be added
+  /// Map new comment model to Json and send it to web services [CommentsWebServices.addComment].
   /// Returns [Comments] which is the model of the new added comment
   Future<Comments> addComment(CommentSubmit commentSubmit) async {
     Map<String, dynamic> jsonMap = commentSubmit.toJson();
